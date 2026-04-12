89b972f8cf19e9a871bfeffebdea1dfc47899362
sugitha99-blip <sugitha99@gmail.com>
2026-04-12 17:13:42 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { Toaster } from './components/ui/sonner';
-import Navigation from './components/custom/Navigation';
-import Hero from './sections/Hero';
-import About from './sections/About';
-import VisionMission from './sections/VisionMission';
-import Children from './sections/Children';
-import Needs from './sections/Needs';
-import Donation from './sections/Donation';
-import Footer from './sections/Footer';
+import { Toaster } from '@/components/ui/sonner';
+import Navigation from '@/components/custom/Navigation';
+import Hero from '@/sections/Hero';
+import About from '@/sections/About';
+import VisionMission from '@/sections/VisionMission';
+import Children from '@/sections/Children';
+import Needs from '@/sections/Needs';
+import Donation from '@/sections/Donation';
+import Footer from '@/sections/Footer';
 
 function App() {
   return (
